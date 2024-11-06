0fbfbb0265707b3618691b15a7dc6195e81c357c
mbenic <mbenic@hotmail.com>
2024-11-06 21:39:00 +0800

    gallery item links

--- app/gallery/page.js
+++ app/gallery/page.js
@@ -1,39 +1,37 @@
-'use client';
-import React from 'react';
-import {useState} from 'react';
-import {sculptureList} from "../data/list"
-import Image from 'next/image';
+"use client";
+import Link from "next/link";
+
+import React from "react";
+import { useState } from "react";
+import { sculptureList } from "../data/list";
+import Image from "next/image";
 
 const Gallery = () => {
-    const [index, setIndex] = useState(0);
+  const [index, setIndex] = useState(0);
 
-    function handleClick() {
-      setIndex((index + 1) % sculptureList.length);
-    }
+  function handleClick() {
+    setIndex((index + 1) % sculptureList.length);
+  }
 
-    let sculpture = sculptureList[index];
-    return (
-      <div className="box">
-        {/* <Header title="New Gallery" /> */}
-        <button className="button is-primary" onClick={handleClick}>
-          Next
-        </button>
-        <h2 className="title is-5">
-          <i>{sculpture.name}</i> by {sculpture.artist}
-        </h2>
-        <h3 className="subtitle is-6">
-          ({index + 1} of {sculptureList.length})
-        </h3>
-        {/* <img src={sculpture.url} alt={sculpture.alt} /> */}
-        <Image
-            src={sculpture.url}
-            alt={sculpture.alt}
-            width={400}
-            height={200}
-        />
-        <p>{sculpture.description}</p>
-      </div>
-    );
+  let sculpture = sculptureList[index];
+  return (
+    <div className="box">
+      {/* <Header title="New Gallery" /> */}
+      <button className="button is-primary" onClick={handleClick}>
+        Next
+      </button>
+      <h2 className="title is-5">
+        <i>{sculpture.name}</i> by {sculpture.artist}
+      </h2>
+      <h3 className="subtitle is-6">
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      {/* <img src={sculpture.url} alt={sculpture.alt} /> */}
+      <Image src={sculpture.url} alt={sculpture.alt} width={400} height={200} />
+      <p>{sculpture.description}</p>
+      <Link href={`/gallery/${sculptureList.id}`}> Go to Item</Link>
+    </div>
+  );
 };
 
 export default Gallery;
